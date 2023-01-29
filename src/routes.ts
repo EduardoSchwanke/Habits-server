fe1a9be467bb2df4793cc2c3bcb25669bc3de804
Eduardo Schwanke <carvalhoe089@gmail.com>
2023-01-29 08:52:53 -0300

    rota de toggle completa

--- src/routes.ts
+++ src/routes.ts
@@ -70,5 +70,54 @@ export async function appRoutes(app: FastifyInstance){
             CompletedHabits
         } 
     })
+
+    app.patch('/habits/:id/toggle', async (request) => {
+        const toggleHabitsParams = z.object({
+            id: z.string().uuid()
+        })
+
+        const { id } = toggleHabitsParams.parse(request.params)
+        const today = dayjs().startOf('day').toDate()
+
+        let day = await prisma.day.findUnique({
+            where: {
+                date: today
+            }
+        })
+
+        if(!day){
+            day = await prisma.day.create({
+                data: {
+                    date: today
+                }
+            })
+        }
+
+        const dayHabit = await prisma.dayHabit.findUnique({
+            where: {
+                day_id_habit_id: {
+                    day_id: day.id,
+                    habit_id: id
+                }
+            }
+        })
+
+        if(dayHabit){
+            await prisma.dayHabit.delete({
+                where: {
+                    id: dayHabit.id
+                }
+            })
+        } else{
+            await prisma.dayHabit.create({
+                data: {
+                    day_id: day.id,
+                    habit_id: id
+                }
+            })
+        }
+
+
+    })
 }
 
